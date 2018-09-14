81233db6d3c2ed441845e562ffefff90f71a8725
jkomal10 <komal.a.jagtap@capgemini.com>
2018-09-14 13:06:57 +0530

    modified by priyanjali main

--- CATTool/src/main/java/com/example/cattool/CatToolApplication.java
+++ CATTool/src/main/java/com/example/cattool/CatToolApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CatToolApplication {
 
 	public static void main(String[] args) {
+	System.out.println("changes by priyanjali");
 		SpringApplication.run(CatToolApplication.class, args);
 	}
 }
