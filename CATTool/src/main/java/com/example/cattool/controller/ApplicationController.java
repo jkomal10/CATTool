c697072791cfeca84641accb4f831fbddff82fae
jkomal10 <komal.a.jagtap@capgemini.com>
2018-09-14 13:23:41 +0530

    jkj

--- CATTool/src/main/java/com/example/cattool/controller/ApplicationController.java
+++ CATTool/src/main/java/com/example/cattool/controller/ApplicationController.java
@@ -1,5 +1,6 @@
 package com.example.cattool.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,4 +16,9 @@ public class ApplicationController {
 		return "Hello word";
 	}
 
+	@GetMapping("/app")
+			public String grt()
+			{
+		return "hjkk";
+			}
 }
